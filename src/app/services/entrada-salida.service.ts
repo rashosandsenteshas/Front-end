575823f40476fdb056ec3a141ce8bba8186cde35
STBenji <michaelgsg16@gmail.com>
2023-03-28 16:34:12 -0500

    final product

--- src/app/services/entrada-salida.service.ts
+++ src/app/services/entrada-salida.service.ts
@@ -1,9 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { environment } from 'src/environments/environment';
-import { Entrada_salida } from '../interfaces/entrada_salida';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
